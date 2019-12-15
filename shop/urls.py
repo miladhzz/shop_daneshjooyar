b9435e464c50da611f99e96544c75fef11536ced
Milad Hatami <miladhzz@gmail.com>
2019-12-15 20:08:28 +0330

    initial templates

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('product/', views.product, name='product'),
+    path('store/', views.store, name='store'),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def product(request):
+    return render(request, 'product.html')
+
+
+def store(request):
+    return render(request, 'store.html')
