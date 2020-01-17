ea887b5e3c380652fce7a2ad7dc4e8b1d22206b8
Milad Hatami <miladhzz@gmail.com>
2020-01-17 18:07:36 +0000

    add models

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'shop'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('checkout/', views.checkout, name='checkout'),
--- website/settings.py
+++ website/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shopdb',
+        'USER': 'postgres',
+        'PASSWORD': 'm123456789*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
